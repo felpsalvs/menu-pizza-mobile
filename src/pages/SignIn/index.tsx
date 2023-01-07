f98eaf9aa074b6e07b7e11163a443225656f0944
felipe <profelipealves@outlook.com>
2023-01-07 09:02:02 -0300

    estiliza tela de login

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,10 +1,66 @@
 import React from "react";
-import { View, Text } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
 
 export default function SignIn() {
   return (
-    <View>
-      <Text>SignIn</Text>
+    <View style={styles.container}>
+      <Image source={require("../../assets/logo.png")} style={styles.logo} />
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.input} placeholder="Digite seu e-mail" placeholderTextColor='#f0f0f0' />
+        <TextInput style={styles.input} placeholder="Digite sua senha" placeholderTextColor='#f0f0f0' />
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Entrar</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#1d1d2c",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  logo: {
+    marginBottom: 20,
+  },
+  inputContainer: {
+    width: "95%",
+    alignItems: "center",
+    justifyContent: "center",
+    paddingVertical: 32,
+    paddingHorizontal: 14,
+  },
+  input: {
+    height: 40,
+    width: "95%",
+    backgroundColor: "#101026",
+    borderRadius: 4,
+    paddingHorizontal: 8,
+    marginBottom: 12,
+    color: "#fff",
+  },
+  button: {
+    height: 40,
+    width: "95%",
+    backgroundColor: "#3fff00",
+    borderRadius: 4,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  buttonText: {
+    color: "#000",
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+});
+
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,15 +1,28 @@
-import React from 'react';
-import {View} from 'react-native';
-import AppRoutes from './app.routes';
-import AuthRoutes from './auth.routes';
+import React from "react";
+import { View, ActivityIndicator } from "react-native";
+import AppRoutes from "./app.routes";
+import AuthRoutes from "./auth.routes";
 
-function Routes(){
-    const isAuthenticated = false;
-    const loading = false;
+function Routes() {
+  const isAuthenticated = false;
+  const loading = false;
 
-    return(
-        isAuthenticated ? <AppRoutes /> : <AuthRoutes />
-    )
+  if (loading) {
+    return (
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+          backgroundColor: "#1d1d2e",
+        }}
+      >
+        <ActivityIndicator size={60} color="#f5f7fb" />
+      </View>
+    );
+  }
+
+  return isAuthenticated ? <AppRoutes /> : <AuthRoutes />;
 }
 
 export default Routes;
