d1920cda894d960c1b3fc901b9f0a26f8b31ab7f
felipe <profelipealves@outlook.com>
2023-01-09 19:02:12 -0300

    add params

--- src/pages/Order/index.tsx
+++ src/pages/Order/index.tsx
@@ -1,11 +1,25 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
+import { useRoute, RouteProp } from "@react-navigation/native";
 
+type RouteDetailParams = {
+  Order:{
+    number: number | string;
+    order_id: string;
+  }
+}
+
+type OrderRouteProps = RouteProp<RouteDetailParams, 'Order'>
 
 export default function Order() {
+  const route = useRoute<OrderRouteProps>();
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tela Order</Text>
+      <Text>
+        {route.params.number}
+      </Text>
     </View>
   );
 }
