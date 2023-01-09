f4331e649cc3e1bd2a9174d4a8f7edb7796d9ddc
felipe <profelipealves@outlook.com>
2023-01-08 22:12:13 -0300

    create Order

---
+++ src/pages/Order/index.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+export default function Order() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Order</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#1d1d2e",
+  },
+  title: {
+    fontSize: 30,
+    color: "#fff",
+    marginBottom: 20,
+    fontWeight: "bold",
+  },
+});
