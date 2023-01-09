0447b63d9d79718a804eb379a20f6a3088c8b2db
felipe <profelipealves@outlook.com>
2023-01-09 19:42:14 -0300

    add style

--- src/pages/Order/index.tsx
+++ src/pages/Order/index.tsx
@@ -1,25 +1,51 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  TextInput,
+} from "react-native";
 import { useRoute, RouteProp } from "@react-navigation/native";
+import { Feather } from "@expo/vector-icons";
 
 type RouteDetailParams = {
-  Order:{
+  Order: {
     number: number | string;
     order_id: string;
-  }
-}
+  };
+};
 
-type OrderRouteProps = RouteProp<RouteDetailParams, 'Order'>
+type OrderRouteProps = RouteProp<RouteDetailParams, "Order">;
 
 export default function Order() {
   const route = useRoute<OrderRouteProps>();
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tela Order</Text>
-      <Text>
-        {route.params.number}
-      </Text>
+      <View style={styles.header}>
+        <Text style={styles.title}>Mesa {route.params.number}</Text>
+        <TouchableOpacity>
+          <Feather name="trash-2" size={28} color="#ff3f4b" />
+        </TouchableOpacity>
+      </View>
+
+      <TouchableOpacity style={styles.input}>
+        <Text style={{ color: "#fff" }}>Adicionar item</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.input}>
+        <Text style={{ color: "#fff" }}>Adicionar item</Text>
+      </TouchableOpacity>
+
+      <View style={styles.qtdContainer}>
+        <Text style={styles.qtdText}>Total: R$ 0,00</Text>
+        <TextInput
+          style={[styles.input, { width: "60%", textAlign: "center" }]}
+          placeholderTextColor="#f0f0f0"
+          keyboardType="numeric"
+          value="0,00"
+        />
+      </View>
     </View>
   );
 }
@@ -27,14 +53,42 @@ export default function Order() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
+    paddingVertical: "5%",
     backgroundColor: "#1d1d2e",
+    paddingEnd: "4%",
+    paddingStart: "4%",
+  },
+  header: {
+    flexDirection: "row",
+    marginBottom: 12,
+    alignItems: "center",
+    marginTop: 24,
   },
   title: {
     fontSize: 30,
     color: "#fff",
-    marginBottom: 20,
+    marginRight: 14,
+    fontWeight: "bold",
+  },
+  input: {
+    backgroundColor: "#101026",
+    borderRadius: 4,
+    width: "100%",
+    height: 40,
+    marginBottom: 12,
+    justifyContent: "center",
+    paddingHorizontal: 8,
+    color: "#fff",
+    fontSize: 20,
+  },
+  qtdContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  qtdText: {
+    color: "#fff",
+    fontSize: 20,
     fontWeight: "bold",
   },
 });
