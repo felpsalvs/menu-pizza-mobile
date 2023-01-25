ede1f0d20d6cb789e6cd29d721365adc6c5af111
Felipe Alves <profelipealves@outlook.com>
2023-01-25 18:59:53 -0300

    feat: update format

--- src/pages/FinishOrder/index.tsx
+++ src/pages/FinishOrder/index.tsx
@@ -1,14 +1,42 @@
 import React from "react";
 import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 import { Feather } from "@expo/vector-icons";
+import { useNavigation, useRoute, RouteProp } from "@react-navigation/native";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack/lib/typescript/src/types";
+import { StackPramsList } from "../../routes/app.routes";
+import { api } from "../../services/api";
+
+type RouteDetailParams = {
+  FinishOrder: {
+    number: number | string;
+    order_id: string;
+  };
+};
+
+type FinisherOrderRouteProps = RouteProp<RouteDetailParams, "FinishOrder">;
 
 export default function FinishOrder() {
+  const navigation = useNavigation<NativeStackNavigationProp<StackPramsList>>();
+  const route = useRoute<FinisherOrderRouteProps>();
+
+  async function handleFinish() {
+    try{
+      await api.put('/order/send', {
+        order_id: route.params?.order_id,
+      })
+
+      navigation.popToTop();
+    }catch(err){
+      console.log('Erro ao finalizar pedido');
+    }
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.alert}>Você deseja finalizar o pedido?</Text>
       <Text style={styles.title}>Mesa 20</Text>
 
-      <TouchableOpacity style={styles.button}>
+      <TouchableOpacity style={styles.button} onPress={handleFinish}>
         <Feather name='shopping-cart' size={20} color="#1d1d2e" />
         <Text style={styles.textButton}>Finalizar pedido</Text>
       </TouchableOpacity>
